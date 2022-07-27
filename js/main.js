09b594824ae7bfe2223a88241ec8b8e943e6d623
Vi <viminehan@gmail.com>
2022-07-27 13:21:29 +1000

    Implemented Overhaul
     - New method for handling page transitions.
     - New story scenarios.
     - More

--- js/main.js
+++ js/main.js
@@ -1,259 +1,178 @@
 /*************************
- *** GLOBAL DECLARATIONS **
+  GLOBAL DECLARATIONS
  *************************/
- gameImage = document.querySelector(".gameImage");
- loadingImage = document.querySelector(".loadingImage");
- let page = 0;
+let currentScenario = 0;
+  
+let scenarioTitle = "";
+let scenarioText = "";
+let gameImageSrc = "";
 
-let intelFollowed = 0;
-let commandsFollowed = 0;
- 
-let intelDisobeyed = 0;
-let commandsDisobeyed = 0;
+let shotsTaken = 0;
+let shotsHeld = 0;
 
-/******************************
- ***** INCREMENTING VALUES*****
- *****************************/
+let gameImage = document.querySelector(".gameImage");
+let loadingImage = document.querySelector(".loadingImage");
 
- function increaseIntelFollowed() {
-    intelFollowed++;
-    return intelFollowed;
- }
+/*************************
+ NAVIGATION
+ ************************/
+ //Home page to instructions
+ document.querySelector(".indexPlayButton").addEventListener('click', playButtonClick);
 
- function increaseIntelDisobeyed() {
-    intelDisobeyed++;
-    return intelDisobeyed;
- }
+ function playButtonClick() {
+    document.querySelector(".homePageClass").style.display = 'none';
+    document.querySelector(".scenariosPageClass").style.display = 'flex';
+};
 
- function increaseCommandsFollowed() {
-    commandsFollowed++;
-    return commandsFollowed
+//Exit button to home
+document.querySelector(".exitButton").addEventListener('click', returnHome);
+
+function returnHome() { 
+    if (confirm("Are you sure you want to exit? Note: This will reset your progress.")) { 
+        location.reload();
+    } else {};
+};
+/******************
+ SCENARIO HANDLING
+ ****************/
+ document.querySelector(".scenarioTakeShotButton").addEventListener('click', runTakeShot);
+ document.querySelector(".scenarioHoldFireButton").addEventListener('click', runHoldFire);
+
+ //Take Shot
+ function runTakeShot () {
+    gameImage.style.display = "none";
+    loadingImage.style.display = "flex";
+    setTimeout(() => { 
+        incrementShotsTaken();
+        loadNextScenario();
+        gameImage.style.display = "flex";
+        loadingImage.style.display = "none";
+    }, 600); 
  };
 
- function increaseCommandsDisobeyed(){
-    commandsDisobeyed++;
-    return commandsDisobeyed;
- }
-
-/*****************************
- ***** PAGE NAVIGATION ******
- ****************************/
-
-//Loading Page Functionality
-function loadNextPage(page) {
-    console.log(page);
+ //Hold Fire
+ function runHoldFire() {
     gameImage.style.display = "none";
-    loadingImage.style.display = "block";
+    loadingImage.style.display = "flex";
     setTimeout(() => { 
-        window.location = page;
+        incrementShotsHeld();
+        loadNextScenario();
+        gameImage.style.display = "flex";
+        loadingImage.style.display = "none";
     }, 600);
+
+ };
+
+ //Load Next Scenario 
+function loadNextScenario() {
+    incrementScenario();
+    selectNewScenario(currentScenario);
+    document.querySelector('.scenarioTitle').innerText = scenarioTitle;
+    document.querySelector('.scenarioText').innerText = scenarioText;
+    document.querySelector('.gameImage').src = gameImageSrc;
+
+    reportValues();
+};
+
+//Report Values
+function reportValues() {
+    console.log(`------- VALUES AS OF STARTING: Scenario ${currentScenario} -------`);
+    console.log(`Shots Taken: ${shotsTaken}`);
+    console.log(`Shots Held: ${shotsHeld}`);
+;} 
+
+//Selecting New Scenario
+function selectNewScenario() {
+    if (currentScenario == 1) {
+        scenarioTitle = 'Scenario One';
+        scenarioText = 'You are perched on the roof of a 10-story building, tasked with providing cover for a squad of soldiers on the ground. They get into a firefight and you target an enemy combatant who is firing on your allies.';
+        gameImageSrc = ""; 
+    } if (currentScenario == 2) {
+        scenarioTitle = 'Scenario Two';
+        scenarioText = 'You move your scope to search for a new target and pass over a friendly solider that you are charged with protecting. ';
+        gameImageSrc = "";  
+    } if (currentScenario == 3) { 
+        scenarioTitle = 'Scenario Three';
+        scenarioText = 'You notice an enemy combatant and line them up in your sights, but notice that they are cowering behind cover and are not actively shooting at your allies.';
+        gameImageSrc = "";
+    } if (currentScenario == 4) { 
+        scenarioTitle = 'Scenario Four';
+        scenarioText = 'An explosion kicks up a massive cloud of dust and it is hard to see the battlefield. You can make out the outline of a solider, but you are only about eighty-percent (80%) certain that it is an enemy combatant.';
+        gameImageSrc = "";
+    } if (currentScenario == 5) { 
+        scenarioTitle = 'Scenario Five';
+        scenarioText = 'The dust settles and the battle is over. The enemy has been eliminated and your forces suffered minimal casualties. You see a lone military-aged male watching from a 5th-story balcony. He makes a phone call.';
+        gameImageSrc = "";
+    } if (currentScenario == 6) {
+        scenarioTitle = 'Scenario Six';
+        scenarioText = "Your team advances through the city and you move to a new building. When you kick down the door of an apartment, a young woman in a floral dress screams at you in a language you don't understand. She is holding a knife.";
+        gameImageSrc = "";
+    } if (currentScenario == 7) {
+        scenarioTitle = 'Scenario Seven';
+        scenarioText = "You take up your new position in the apartment. Your allies are moving up the street. You spot a military-aged male in a sidestreet, holding an assault rifle.";
+        gameImageSrc = "";
+    } if (currentScenario == 8) {
+        scenarioTitle = 'Scenario Eight';
+        scenarioText = "Your presence in this place is not welcome. Civilians in second-floor apartments hurl curse words and stones at your allies.";
+        gameImageSrc = "";
+    } if (currentScenario == 9) {
+        scenarioTitle = 'Scenario Nine';
+        scenarioText = "You spot movement on the roof of another building, an enemy combatant wielding a rocket launcher is taking aim at your troops.";
+        gameImageSrc = "";
+    } if (currentScenario == 10) {
+        scenarioTitle == 'Scenario Ten';
+        scenarioText = "The team creeps up the main street. A women with a child close by, approaches your allies - it appears she is saying something. Her hand is closed around something.";
+        gameImageSrc = "";
+    } if (currentScenario == 11) {
+        scenarioTitle = 'Scenario Eleven';
+        scenarioText = "Your team reaches the target location and starts to assemble cover. Outside an old car carrying two passengers, travels at speed down the road towards the building.";
+        gameImageSrc = "";
+    } if (currentScenario == 12) {
+        scenarioTitle = 'Scenario Twelve';
+        scenarioText = "You spot an enemy sniper who has lined you up in their sights. Taking the shot would risk being fired upon yourself, but holding your fire and taking cover could mean that the sniper repositions and targets your team.";
+        gameImageSrc = "";
+    } if (currentScenario == 13) {
+        scenarioTitle = 'Scenario Thirteen';
+        scenarioText = "A crowd of civilian protestors marches down the street towards the target location.. Amongst their ranks you spot a man obscuring an explosive. Taking the shot may result in the death of a civilian if they get in the way.";
+        gameImageSrc = "";
+    } if (currentScenario == 14) {
+        scenarioTitle = 'Scenario Fourteen';
+        scenarioText = "A woman in the crowd draws a rifle and starts firing at your perch. She is using another woman as a bodyshield. Taking the shot would mean killing both the combatant and the innocent woman.";
+        gameImageSrc = "";
+    } if (currentScenario == 15) { 
+        scenarioTitle = 'Scenario Fifteen';
+        scenarioText = "The mission is almost complete. On the ground level, of a home reduced to rubble - you spot a young man, clutching the body of his bride.";
+        gameImageSrc = "";
+    } if (currentScenario == 16) {
+        console.log('End of Scenarios...');
+        moveToResults();
+    } else {
+        console.log('selectNewScenario() Error');
+    };
+};
+
+function moveToResults() {
+    console.log('Moved to results.');
 }
- //Move from index.html to instructions.html
-if (document.URL.includes("index.html")) {
-    document.querySelector(".indexPlayButton").addEventListener('click', playButtonClick);
 
-    function playButtonClick () {
-        console.log('Loading Instructions Page...');
-        window.location = "instructions.html";
-    }
-}
+/***********************
+ INCREMENTING VALUES
+ ************************/
 
-//Exit Button moves user from any page to index.html
-document.querySelector(".exitButton").addEventListener('click', exitButtonClick);
-
-function exitButtonClick() {
-    console.log('Exiting Game...');
-    window.location = "index.html";
-    intelDisobeyed = 0;
-    intelFollowed = 0;
-    commandsDisobeyed = 0;
-    commandsFollowed = 0; 
-}
-
-//Move from instructions.html to scenarioone.html
-if (document.URL.includes("instructions.html")) {
-    document.querySelector(".takeShotInstructions").addEventListener('click', takeShotInstructionsClick);
-    document.querySelector(".holdFireInstructions").addEventListener('click', holdFireInstructionsClick);
-
-     function takeShotInstructionsClick () {
-        page = "scenarioone.html";
-        loadNextPage(page);
-    };
-
-    function holdFireInstructionsClick() {
-        alert("to continue, please click 'Take Shot '");
-    };
-}
-
-//Move from scenarioone.html to scenariotwo.html
-if (document.URL.includes("scenarioone.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseIntelFollowed();
-        increaseCommandsFollowed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseIntelDisobeyed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenariotwo.html";
-        loadNextPage(page);
-     };
+//Increment Scenario
+function incrementScenario() { 
+    currentScenario++;
+    return currentScenario;
 };
 
-//Move from scenariotwo.html to scenariothree.html
-if (document.URL.includes("scenariotwo.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseIntelDisobeyed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseIntelFollowed();
-        increaseCommandsFollowed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenariothree.html";
-        loadNextPage(page);
-     };
+//Increment Shots Taken
+function incrementShotsTaken() {
+    if (currentScenario > 0) { 
+        shotsTaken++; } else {};
 };
 
-//Move from scneariothree.html to scenariofour.html
-if (document.URL.includes("scenariothree.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseIntelFollowed();
-        increaseCommandsFollowed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseIntelDisobeyed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenariofour.html";
-        loadNextPage(page);
-     };
+//Increment Shots Held
+function incrementShotsHeld() { 
+    if (currentScenario > 0) { 
+        shotsHeld++; } else {}; 
 };
-
-//Move from scneariofour.html to scenariofive.html
-if (document.URL.includes("scenariofour.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseCommandsFollowed();
-        increaseIntelDisobeyed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseIntelFollowed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenariofive.html";
-        loadNextPage(page);
-     };
-};
-
-//Move from scenariofive.html to scenariosix.html
-if (document.URL.includes("scenariofive.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseIntelFollowed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseCommandsFollowed();
-        increaseIntelDisobeyed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenariosix.html";
-        loadNextPage(page);
-     };
-};
-
-//Move from scenariosix.html to scenarioseven.html
-if (document.URL.includes("scenariosix.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseIntelFollowed();
-        increaseCommandsFollowed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseIntelDisobeyed();
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "scenarioseven.html";
-        loadNextPage(page);
-     };
-};
-
-//Move from scenarioseven.html to results.html
-if (document.URL.includes("scenarioseven.html")) {
-    document.querySelector(".takeShot").addEventListener('click', takeShot);
-    document.querySelector(".holdFire").addEventListener('click', holdFire);
-
-    function takeShot() { 
-        increaseCommandsDisobeyed();
-        advanceToNextPage();
-    };
-
-    function holdFire() {
-        increaseCommandsFollowed();
-        advanceToNextPage();
-    };
-
-    function advanceToNextPage() {
-        page = "results.html";
-        loadNextPage(page);
-     };
-};
-
-//Results Page
-if (document.URL.includes("results.html")) {
- console.log(`Commands Followed: ${commandsFollowed}, Commands Disobeyed: ${commandsDisobeyed}, Intel Followed: ${intelFollowed}, Intel Disobeyed: ${intelDisobeyed}`);
-}
-/*******************
- ***** BACKGROUND ****
-********************/
-
-/* html { 
-  background: rgb(111,16,16);
-background: radial-gradient(circle, rgba(111,16,16,1) 0%, rgba(255,255,255,0) 100%);
-}
-*/ 
